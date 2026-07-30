4f23c4e0f53d5892ba3ef8e2973129521322c9e7
omurfm50 <omurfm50@gmail.com>
2026-07-30 11:49:49 +0300

    Remove room sidebar cards

--- src/components/RoomLayout.jsx
+++ src/components/RoomLayout.jsx
@@ -1,21 +1,13 @@
 import { useChatUserCount } from '../hooks/useChatUserCount.js'
 import ChattersNetRoom from './ChattersNetRoom.jsx'
-import CurrentDjCard from './CurrentDjCard.jsx'
-import OnlineSummaryCard from './OnlineSummaryCard.jsx'
-import RoomInfoCard from './RoomInfoCard.jsx'
 
 function RoomLayout({ onOpenRules }) {
   const userCountState = useChatUserCount()
 
   return (
     <section id="ana-oda" className="scroll-mt-20 py-5 sm:py-6">
-      <div className="grid items-start gap-4 lg:grid-cols-[minmax(0,3fr)_minmax(240px,1fr)]">
+      <div>
         <ChattersNetRoom {...userCountState} refreshUserCount={userCountState.refresh} onOpenRules={onOpenRules} />
-        <aside className="grid gap-4 sm:grid-cols-2 lg:grid-cols-1">
-          <CurrentDjCard />
-          <OnlineSummaryCard userCount={userCountState.userCount} isLoading={userCountState.isLoading} />
-          <RoomInfoCard />
-        </aside>
       </div>
     </section>
   )
