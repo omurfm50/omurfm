c58688ec9fd0e2b449db0b150bab740e902a6bf2
omurfm50 <omurfm50@gmail.com>
2026-08-01 15:22:35 +0300

    Restore Caster.fm radio player

--- src/components/StickyRadioBar.jsx
+++ src/components/StickyRadioBar.jsx
@@ -1,7 +1,11 @@
-import FlatcastRadio from './FlatcastRadio.jsx'
+import CasterWidget from './CasterWidget.jsx'
 
 function StickyRadioBar() {
-  return <FlatcastRadio />
+  return (
+    <section id="canli-yayin" aria-label="Ömür FM canlı yayın oynatıcısı" className="scroll-mt-20 py-6">
+      <CasterWidget />
+    </section>
+  )
 }
 
 export default StickyRadioBar
