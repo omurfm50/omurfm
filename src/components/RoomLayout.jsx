8fce71295a05822b33fc63be761c518b1a67a007
omurfm50 <omurfm50@gmail.com>
2026-08-04 11:00:22 +0300

    Remove chat room header

--- src/components/RoomLayout.jsx
+++ src/components/RoomLayout.jsx
@@ -1,13 +1,10 @@
-import { useChatUserCount } from '../hooks/useChatUserCount.js'
 import ChattersNetRoom from './ChattersNetRoom.jsx'
 
 function RoomLayout() {
-  const userCountState = useChatUserCount()
-
   return (
     <section id="ana-oda" className="-mx-3 scroll-mt-20 py-5 sm:mx-0 sm:py-6">
       <div>
-        <ChattersNetRoom {...userCountState} refreshUserCount={userCountState.refresh} />
+        <ChattersNetRoom />
       </div>
     </section>
   )
