c9de236aa31a19b72f16a9414a8562b0ea34f693
omurfm50 <omurfm50@gmail.com>
2026-08-05 16:00:55 +0300

    Masaüstü oynatıcıyı biraz daha kısalt

--- src/components/StickyRadioBar.jsx
+++ src/components/StickyRadioBar.jsx
@@ -7,7 +7,7 @@ function StickyRadioBar() {
       <section
         id="canli-yayin"
         aria-label="Ömür FM canlı yayın oynatıcısı"
-        className="scroll-mt-20 py-6 sm:fixed sm:inset-x-5 sm:bottom-3 sm:z-50 sm:mx-auto sm:max-w-[840px] sm:py-0"
+        className="scroll-mt-20 py-6 sm:fixed sm:inset-x-5 sm:bottom-3 sm:z-50 sm:mx-auto sm:max-w-[714px] sm:py-0"
       >
         <CasterWidget />
       </section>
